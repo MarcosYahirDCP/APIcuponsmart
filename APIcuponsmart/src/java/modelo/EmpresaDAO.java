fa4ba7c7c31186486d451d07be04864b96e58b92
BrandonVasquezL <zs20022115@estudiantes.uv.mx>
2023-11-11 01:13:13 -0600

    Servicio Empresa

--- APIcuponsmart/src/java/modelo/EmpresaDAO.java
+++ APIcuponsmart/src/java/modelo/EmpresaDAO.java
@@ -8,37 +8,145 @@ import org.apache.ibatis.session.SqlSession;
 public class EmpresaDAO {
     
     public static Mensaje registrarEmpresa(Empresa empresa){
-        Mensaje mensaje = new Mensaje();
+        Mensaje msj = new Mensaje();
+        msj.setError(true);
         SqlSession conexionBD = MyBatisUtil.getSession();
         if(conexionBD != null){
             try{
                 Empresa rfc = conexionBD.selectOne("empresa.empresaPorRFC", empresa.getRFC());
                 if(rfc != null){
-                    mensaje.setError(true);
-                    mensaje.setMensaje("El rfc ya se encuentra registrado en una empresa");
+                    msj.setMensaje("El rfc ya se encuentra registrado en una empresa");
                     conexionBD.close();
-                    return mensaje;
+                    return msj;
                 }
                 int filasAfectadas = conexionBD.insert("empresa.registrarEmpresa", empresa);
                 conexionBD.commit();
                 if(filasAfectadas > 0){
-                    mensaje.setError(false);
-                    mensaje.setMensaje("Empresa registrada exitosamente, " + filasAfectadas + " filas afectadas.");
+                    msj.setError(false);
+                    msj.setMensaje("Empresa registrada exitosamente, " + filasAfectadas + " filas afectadas.");
                 }else{
-                    mensaje.setError(true);
-                    mensaje.setMensaje("Error al guardar los datos, pruebe mas tarde.");
+                    msj.setMensaje("Error al guardar los datos, pruebe mas tarde.");
                 }
             }catch(Exception e){
                 e.printStackTrace();
-                mensaje.setError(true);
-                mensaje.setMensaje("Error al ejecutar la operacion." + e);
+                msj.setMensaje("Error al ejecutar la operacion." + e);
             }finally{
                 conexionBD.close();
             }
         }else{
-            mensaje.setError(true);
-            mensaje.setMensaje("No hay conexion a la BD, prueba mas tarde.");
+            msj.setMensaje("No hay conexion a la BD, prueba mas tarde.");
         }
-        return mensaje;
+        return msj;
+    }
+    
+    public static Mensaje editarEmpresa(Empresa empresa, String RFC){
+        Mensaje msj = new Mensaje();
+        msj.setError(true);
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                Empresa empresaExiste = conexionBD.selectOne("empresa.empresaPorRFC",RFC);
+                if(empresaExiste != null){
+                   int filasAfectadas = conexionBD.update("empresa.editarEmpresa", empresa);
+                    conexionBD.commit();
+                    if(filasAfectadas > 0){
+                        msj.setError(false);
+                        msj.setMensaje("Empresa editada exitosamente, " + filasAfectadas + " filas afectadas.");
+                    }else{
+                        msj.setMensaje("Error al guardar los datos, pruebe mas tarde.");
+                    } 
+                }else{
+                    msj.setMensaje("La empresa con el RFC: "+RFC + " no existe");
+                }
+            }catch(Exception e){
+                msj.setMensaje("Error al ejecutar la operacion." + e);
+            }finally{
+                conexionBD.close();
+            }
+        }else{
+          msj.setMensaje("No hay conexion a la BD, prueba mas tarde."); 
+        }
+        return msj;
+    }
+    
+    public static Mensaje eliminarEmpresa(String RFC){
+        Mensaje msj = new Mensaje();
+        msj.setError(true);
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                Empresa empresaExiste = conexionBD.selectOne("empresa.empresaPorRFC",RFC);
+                if(empresaExiste != null){
+                   int filasAfectadas = conexionBD.delete("empresa.eliminarEmpresa", RFC);
+                    conexionBD.commit();
+                    if(filasAfectadas > 0){
+                        msj.setError(false);
+                        msj.setMensaje("Empresa eliminada exitosamente, " + filasAfectadas + " filas afectadas.");
+                    }else{
+                        msj.setMensaje("Error al procesae los datos, pruebe mas tarde.");
+                    } 
+                }else{
+                    msj.setMensaje("La empresa con el RFC: "+RFC + " no existe");
+                }
+            }catch(Exception e){
+                msj.setMensaje("Error al ejecutar la operacion." + e);
+            }finally{
+                conexionBD.close();
+            }
+        }else{
+          msj.setMensaje("No hay conexion a la BD, prueba mas tarde."); 
+        }
+        return msj;
+    }
+    
+    public static Empresa empresaPorRFC(String RFC){
+        Empresa empresa = null;
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                empresa = conexionBD.selectOne("empresa.empresaPorRFC", RFC);
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                conexionBD.close();
+            }
+        }else{
+            
+        }
+        return empresa;
+    }
+    
+    public static Empresa empresaPorNombreComercial(String nombreComercial){
+        Empresa empresa = null;
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                empresa = conexionBD.selectOne("empresa.empresaPorNombreComercial", nombreComercial);
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                conexionBD.close();
+            }
+        }else{
+            
+        }
+        return empresa;
+    }
+    
+    public static Empresa empresaPorRepresentante(String representante){
+        Empresa empresa = null;
+        SqlSession conexionBD = MyBatisUtil.getSession();
+        if(conexionBD != null){
+            try{
+                empresa = conexionBD.selectOne("empresa.empresaPorRepresentante", representante);
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                conexionBD.close();
+            }
+        }else{
+            
+        }
+        return empresa;
     }
 }
--- APIcuponsmart/src/java/ws/EmpresaWS.java
+++ APIcuponsmart/src/java/ws/EmpresaWS.java
@@ -3,7 +3,11 @@ package ws;
 import com.google.gson.Gson;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
@@ -42,5 +46,86 @@ public class EmpresaWS {
             msj.setMensaje("Error "+ e.getMessage());
         }
         return msj;
+    }
+    
+    @PUT
+    @Path("editarEmpresa")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Mensaje editarEmpresa(String jsonParam){
+     Mensaje msj = new Mensaje();
+         try{
+            Gson gson = new Gson();
+            Empresa empresa = gson.fromJson(jsonParam, Empresa.class);
+            if(empresa != null && empresa.getRFC() != null){
+                msj = EmpresaDAO.editarEmpresa(empresa, empresa.getRFC());
+            }else{
+                throw new WebApplicationException(Response.Status.BAD_REQUEST);
+            }
+        }catch(Exception e){
+            msj.setError(true);
+            msj.setMensaje("Error "+ e.getMessage());
+        }
+         return msj;
+    }
+    
+    @DELETE
+    @Path("eliminarEmpresa")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Mensaje eliminarEmpresa(String jsonParam){
+     Mensaje msj = new Mensaje();
+         try{
+            Gson gson = new Gson();
+            Empresa empresa = gson.fromJson(jsonParam, Empresa.class);
+            if(empresa != null && empresa.getRFC() != null){
+                msj = EmpresaDAO.eliminarEmpresa(empresa.getRFC());
+            }else{
+                throw new WebApplicationException(Response.Status.BAD_REQUEST);
+            }
+        }catch(Exception e){
+            msj.setError(true);
+            msj.setMensaje("Error "+ e.getMessage());
+        }
+         return msj;
+    }
+    
+    @GET
+    @Path("empresaRFC/{RFC}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Empresa empresaPorRFC(@PathParam("RFC") String RFC){
+        Empresa empresa = null;
+        if(RFC != null){
+            empresa =  EmpresaDAO.empresaPorRFC(RFC);
+        }else{
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+        return empresa;
+    }
+    
+    @GET
+    @Path("empresaNombreComercial/{nombreComercial}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Empresa empresaPorNombreComercial(@PathParam("nombreComercial") String nombreComercial){
+        Empresa empresa = null;
+        if(nombreComercial != null){
+            empresa =  EmpresaDAO.empresaPorNombreComercial(nombreComercial);
+        }else{
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+        return empresa;
+    }
+    
+    @GET
+    @Path("empresaRepresentante/{representante}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Empresa empresaPorRepresentante(@PathParam("representante") String representante){
+        Empresa empresa = null;
+        if(representante != null){
+            empresa =  EmpresaDAO.empresaPorRepresentante(representante);
+        }else{
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+        return empresa;
     }
 }
